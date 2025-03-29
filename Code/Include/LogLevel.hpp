dcfe019c62156cd896a6c6e46ec27ef48b9f9a74
Ethan Ruhlmann <e.ruhlmann@student.isartdigital.com>
2025-03-29 22:43:20 +0100

    remove: Unused feature

--- Code/Include/LogLevel.hpp
+++ Code/Include/LogLevel.hpp
@@ -10,37 +10,4 @@ namespace Debug
         ERROR = 3,
         CRITICAL = 4
     };
-
-
-    enum class LogFlag : unsigned int
-    {
-        None = 0,
-        NoFunc = 1 << 0,
-        NoTimestamp = 1 << 1,
-        NoSource = 1 << 2,
-        NoLevel = 1 << 3
-    };
-
-
-    inline LogFlag operator|(LogFlag a_leftString, LogFlag a_rightString)
-    {
-        return static_cast<LogFlag>(static_cast<uint8_t>(a_leftString) | static_cast<uint8_t>(a_rightString));
-    }
-
-    inline LogFlag operator&(LogFlag a_lString, LogFlag a_rightString)
-    {
-        return static_cast<LogFlag>(static_cast<uint8_t>(a_lString) & static_cast<uint8_t>(a_rightString));
-    }
-
-    inline LogFlag& operator|=(LogFlag& a_lString, LogFlag a_rightString)
-    {
-        a_lString = a_lString | a_rightString;
-        return a_lString;
-    }
-
-    inline LogFlag& operator&=(LogFlag& a_lString, LogFlag a_rightString)
-    {
-        a_lString = a_lString & a_rightString;
-        return a_lString;
-    }
 }
