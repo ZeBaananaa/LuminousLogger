75b9f6a40c291ae9f663513f26b14335649179b3
Ethan Ruhlmann <e.ruhlmann@student.isartdigital.com>
2025-05-07 14:06:48 +0200

    fix: Use proper memory order to avoid potential issues + small cleanup

--- Code/Source/LockFreeQueue.cpp
+++ Code/Source/LockFreeQueue.cpp
@@ -10,13 +10,13 @@ namespace Debug
         size_t l_nextTail{};
         do
         {
-            l_currentTail = m_tail.load(std::memory_order_relaxed);
+            l_currentTail = m_tail.load(std::memory_order_acquire);
             l_nextTail = (l_currentTail + 1) & (m_capacity - 1);
 
             if (l_nextTail == m_head.load(std::memory_order_acquire))
                 return false;
         }
-        while (!m_tail.compare_exchange_weak(l_currentTail, l_nextTail, std::memory_order_release, std::memory_order_relaxed));
+        while (!m_tail.compare_exchange_weak(l_currentTail, l_nextTail, std::memory_order_acq_rel));
 
         m_data[l_currentTail] = a_log;
         return true;
@@ -27,12 +27,12 @@ namespace Debug
         size_t l_currentHead{};
         do
         {
-            l_currentHead = m_head.load(std::memory_order_relaxed);
+            l_currentHead = m_head.load(std::memory_order_acquire);
 
             if (l_currentHead == m_tail.load(std::memory_order_acquire))
                 return std::nullopt;
 
-        } while (!m_head.compare_exchange_weak(l_currentHead, (l_currentHead + 1) & (m_capacity - 1), std::memory_order_release, std::memory_order_relaxed)); // Bitwise operation used for better optimization than the '%' sign (only when queue capacity is a power of 2)
+        } while (!m_head.compare_exchange_weak(l_currentHead, (l_currentHead + 1) & (m_capacity - 1), std::memory_order_acq_rel)); // Bitwise operation used for better optimization than the '%' sign (only when queue capacity is a power of 2)
         return m_data[l_currentHead];
     }
 }
